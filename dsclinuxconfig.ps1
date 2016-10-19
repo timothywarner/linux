d1345f11763ff9aa0f739e03f630f7f6009f78ad
Timothy Warner <timothywarner316@gmail.com>
2016-10-19 14:27:58 -0700

    add to repo

---
+++ dsclinuxconfig.ps1
@@ -0,0 +1,31 @@
+Set-Location -Path 'C:\Users\trainer\Documents\LinuxDSC'
+
+Configuration ExampleConfiguration {
+
+    Import-DscResource -Module nx
+
+    Node  "linux1" {
+        nxFile ExampleFile {
+
+            DestinationPath = "/tmp/example"
+            Contents = "Hello world! `n"
+            Ensure = "Present"
+            Type = "File"
+        }
+
+    }
+}
+
+ExampleConfiguration -OutputPath:"C:\temp"
+
+$Node = "linux1"
+$Credential = Get-Credential -UserName:"root" -Message:"Enter Linux Root Password:"
+
+#Ignore SSL certificate validation
+$opt = New-CimSessionOption -UseSsl:$true -SkipCACheck:$true -SkipCNCheck:$true -SkipRevocationCheck:$true
+
+#Options for a trusted SSL certificate
+$opt = New-CimSessionOption -UseSsl:$true 
+$Sess=New-CimSession -Credential:$credential -ComputerName:$Node -Port:5986 -Authentication:basic -SessionOption:$opt -OperationTimeoutSec:90
+
+Start-DscConfiguration -Path:"C:\temp" -CimSession:$Sess -Wait -Verbose
