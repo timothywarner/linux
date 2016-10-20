9b08c25218ecf7fbdb6b1bbd807b9cbefc709913
Timothy Warner <timothywarner316@gmail.com>
2016-10-20 05:45:39 -0700

    Tweak the session configuration options

--- dsclinuxconfig.ps1
+++ dsclinuxconfig.ps1
@@ -26,14 +26,18 @@ Configuration ExampleConfiguration {
 
 ExampleConfiguration -OutputPath:"C:\temp"
 
+Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value *
+
+Set-Item -Path WSMan:\localhost\Client\AllowUnencrypted -Value $true
+
 $Node = "linux1"
 $Credential = Get-Credential -UserName:"root" -Message:"Enter Linux Root Password:"
 
 #Ignore SSL certificate validation
-$opt = New-CimSessionOption -UseSsl:$true -SkipCACheck:$true -SkipCNCheck:$true -SkipRevocationCheck:$true
+$opt = New-CimSessionOption -UseSsl:$false -SkipCACheck:$true -SkipCNCheck:$true -SkipRevocationCheck:$true
 
 #Options for a trusted SSL certificate
-$opt = New-CimSessionOption -UseSsl:$true 
+$opt = New-CimSessionOption -UseSsl:$false 
 $Sess=New-CimSession -Credential:$credential -ComputerName:$Node -Port:5986 -Authentication:basic -SessionOption:$opt -OperationTimeoutSec:90
 
 Start-DscConfiguration -Path:"C:\temp" -CimSession:$Sess -Wait -Verbose
