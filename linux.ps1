18098d99a01341b4825f0f0d55c3a8a0dac57d71
= <timothywarner316@gmail.com>
2016-10-25 10:58:36 -0500

    Tweak Automation DSC commands

--- linux.ps1
+++ linux.ps1
@@ -16,6 +16,8 @@ yum groupinstall 'Development Tools'
 yum install pam-devel
 yum install openssl-devel
 yum install wget
+yum install python
+yum install python-devel
 
 #endregion
 
@@ -63,23 +65,15 @@ sudo /opt/omi/bin/omicli ei root/omi OMI_Identify
 
 #region Install DSC on Linux
 
-# the easy way
 Start-Process -FilePath https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/
 
-sudo rpm -Uvh dsc-1.0.0-254.ssl_100.x64.rpm
-
-# the hard way
-yum install python
-yum install python-devel
-cd /root/downloads
 wget https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_100.x64.rpm
-sudo rpm -Uvh dsc-1.1.1-294.ssl_098.x64.rpm
 
-https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_098.x64.rpm
+sudo rpm -Uvh ./dsc-1.1.1-294.ssl_100.x64.rpm
 
 #endregion
 
-#region Local configuration
+#region Local LCM configuration
 
 cd /opt/microsoft/dsc/Scripts
 ls -1 *.py
@@ -102,7 +96,9 @@ sudo ./SetDscLocalConfigurationManager.py –configurationmof /tmp/localhost.meta.
 
 Start-Process -FilePath https://github.com/PowerShell/PowerShell/releases/
 
-sudo yum install ./powershell-6.0.0_alpha.11-1.el7.centos.x86_64.rpm
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.11/powershell-6.0.0_alpha.11-1.el7.centos.x86_64.rpm
+
+sudo yum install -y ./powershell-6.0.0_alpha.11-1.el7.centos.x86_64.rpm
 
 powershell
 
--- onboardlinuxvm.ps1
+++ onboardlinuxvm.ps1
@@ -1,1 +1,33 @@
-﻿
+﻿#region Connect to Your Azure Subscription
+# ARM
+$defaultStorageAccount = 'itedgestorage'
+$defaultResourceGroup = 'ITEdgeRG'
+
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName '150dollar'
+Set-AzureRmCurrentStorageAccount -ResourceGroupName $defaultResourceGroup -StorageAccountName $defaultStorageAccount 
+Set-AzureRmContext -SubscriptionName '150dollar'
+
+#endregion
+
+#region Onboard an Azure Linux VM
+
+# onboarding doc
+Start-Process -FilePath https://azure.microsoft.com/en-us/documentation/articles/automation-dsc-onboarding/
+
+# This doesn't seem to work (GRRR)
+Register-AzureRmAutomationDscNode -AutomationAccountName 'itedgedsc' `
+ -AzureVMName 'itedgelinux1' -ResourceGroupName 'ITEdgeRG' `
+ -NodeConfigurationName 'SampleConfig1.demo' -Verbose
+
+# Try this ARM template instead:
+Start-Process -FilePath https://azure.microsoft.com/en-us/documentation/templates/dsc-extension-azure-automation-pullserver/
+
+# Or do a manual registration from inside the Linux Azure VM:
+cd /opt/microsoft/dsc/Scripts
+sudo ./Register.py <accountkey> <accountURL>
+
+#endregion
+
+
+
